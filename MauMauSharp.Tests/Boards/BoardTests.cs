31f362e651135f9440195cc5c6540e6c585f431c
Dennis Renz <dennis@r-renz.de>
2021-04-06 00:28:18 +0200

    Add board initialization test

--- MauMauSharp.Tests/Boards/BoardTests.cs
+++ MauMauSharp.Tests/Boards/BoardTests.cs
@@ -1,10 +1,27 @@
-﻿using NUnit.Framework;
+﻿using MauMauSharp.Boards;
+using MauMauSharp.Cards;
+using MauMauSharp.Cards.Enums;
+using MauMauSharp.TestUtilities.Mocks.Cards.Shufflers;
+using NUnit.Framework;
 
 namespace MauMauSharp.Tests.Boards
 {
     [TestFixture]
     public class BoardTests
     {
-        
+        [Test]
+        public void Cards_Are_Shuffled_And_The_Top_Card_Revealed_On_A_Fresh_Board()
+        {
+            var board = new Board(
+                // TODO: Parser for easy creation
+                new Card[]
+                {
+                    new(Rank.King, Suit.Clubs),
+                    new(Rank.King, Suit.Diamonds)
+                },
+                ShufflerMocks.Reversing<Card>().Object);
+
+            Assert.That(board.TopCard(), Is.EqualTo(new Card(Rank.King, Suit.Clubs)));
+        }
     }
 }
--- MauMauSharp/Boards/Board.cs
+++ MauMauSharp/Boards/Board.cs
@@ -1,7 +1,27 @@
-﻿namespace MauMauSharp.Boards
+﻿using System.Collections.Generic;
+using MauMauSharp.Cards;
+using MauMauSharp.Cards.Shufflers;
+
+namespace MauMauSharp.Boards
 {
     public class Board
     {
-        
+        private readonly IShuffler _shuffler;
+
+        // TODO: Better names!
+        private readonly Stack<Card> _discard;
+        private readonly Stack<Card> _deck;
+
+        public Board(IEnumerable<Card> cards, IShuffler shuffler)
+        {
+            _shuffler = shuffler;
+
+            _deck = new(_shuffler.Shuffle(cards));
+            _discard = new();
+            _discard.Push(_deck.Pop());
+        }
+
+        // TODO: Better name?
+        public Card TopCard() => _discard.Peek();
     }
 }
