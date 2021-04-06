68b16bfe2b5126f4d5da32d7df00cf689b68886b
Dennis Renz <dennis@r-renz.de>
2021-04-06 17:36:29 +0200

    Refactor Encoding and Grammer for cleaner code

--- MauMauSharp.TestUtilities/Parsers/Cards/Encoding.cs
+++ MauMauSharp.TestUtilities/Parsers/Cards/Encoding.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using JetBrains.Annotations;
 using MauMauSharp.Cards.Enums;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace MauMauSharp.TestUtilities.Parsers.Cards
 {
+    [PublicAPI]
     public static class Encoding
     {
         public static char RankEncoding(Rank rank)
@@ -19,6 +23,14 @@ namespace MauMauSharp.TestUtilities.Parsers.Cards
                 _ => throw new ArgumentException($"Can't encode unknown rank: {rank}")
             };
 
+        public static ImmutableArray<(Rank, char)> AllRanksWithEncodings { get; }
+            = Enum
+                .GetValues<Rank>()
+                .Zip(Enum
+                    .GetValues<Rank>()
+                    .Select(RankEncoding))
+                .ToImmutableArray();
+
         public static char SuitEncoding(Suit suit)
             => suit switch
             {
@@ -28,5 +40,13 @@ namespace MauMauSharp.TestUtilities.Parsers.Cards
                 Suit.Clubs => 'c',
                 _ => throw new ArgumentException($"Can't encode unknown suit: {suit}")
             };
+
+        public static ImmutableArray<(Suit, char)> AllSuitsWithEncodings { get; }
+            = Enum
+                .GetValues<Suit>()
+                .Zip(Enum
+                    .GetValues<Suit>()
+                    .Select(SuitEncoding))
+                .ToImmutableArray();
     }
 }
--- MauMauSharp.TestUtilities/Parsers/Cards/Grammar.cs
+++ MauMauSharp.TestUtilities/Parsers/Cards/Grammar.cs
@@ -1,25 +1,28 @@
 ﻿using MauMauSharp.Cards;
 using MauMauSharp.Cards.Enums;
 using Sprache;
-using System;
 using System.Linq;
 
 namespace MauMauSharp.TestUtilities.Parsers.Cards
 {
     public static class Grammar
     {
+        private static Parser<T> FromCharEncoding<T>(T t, char encoding)
+            => Parse.Char(encoding).Return(t);
+
+        private static Parser<T> FromCharEncoding<T>((T Value, char Encoding) valueAndEncoding)
+            => FromCharEncoding(valueAndEncoding.Value, valueAndEncoding.Encoding);
+
         private static Parser<Rank> Rank()
-            => Enum
-                .GetValues<Rank>()
-                .Zip(Enum.GetValues<Rank>().Select(Encoding.RankEncoding))
-                .Select(rankAndEncoding => Parse.Char(rankAndEncoding.Second).Return(rankAndEncoding.First))
+            => Encoding
+                .AllRanksWithEncodings
+                .Select(FromCharEncoding)
                 .Aggregate(Parse.Or);
 
         private static Parser<Suit> Suit()
-            => Enum
-                .GetValues<Suit>()
-                .Zip(Enum.GetValues<Suit>().Select(Encoding.SuitEncoding))
-                .Select(suitAndEncoding => Parse.Char(suitAndEncoding.Second).Return(suitAndEncoding.First))
+            => Encoding
+                .AllSuitsWithEncodings
+                .Select(FromCharEncoding)
                 .Aggregate(Parse.Or);
 
         public static Parser<Card> Card() =>
