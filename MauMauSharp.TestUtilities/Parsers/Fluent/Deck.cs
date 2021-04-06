046d4e765fe7a0dae047542335b2a69cafa61e17
Dennis Renz <dennis@r-renz.de>
2021-04-06 16:01:13 +0200

    Add fluent syntax to parse stack of cards

---
+++ MauMauSharp.TestUtilities/Parsers/Fluent/Deck.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using MauMauSharp.Cards;
+using MauMauSharp.TestUtilities.Parsers.Cards;
+using Sprache;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MauMauSharp.TestUtilities.Parsers.Fluent
+{
+    [PublicAPI]
+    public static class Deck
+    {
+        public static IEnumerable<Card> FromStacked(params string[] cards)
+            => cards.Select(card => Grammar.Card().Parse(card));
+    }
+}
