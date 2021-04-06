2077bf3cc8ab206d07860bf81547a1761c61626c
Dennis Renz <dennis@r-renz.de>
2021-04-06 16:03:49 +0200

    Use fluent syntax in test

--- MauMauSharp.Tests/Boards/BoardTests.cs
+++ MauMauSharp.Tests/Boards/BoardTests.cs
@@ -2,7 +2,10 @@
 using MauMauSharp.Cards;
 using MauMauSharp.Cards.Enums;
 using MauMauSharp.TestUtilities.Mocks.Cards.Shufflers;
+using MauMauSharp.TestUtilities.Parsers.Cards;
+using MauMauSharp.TestUtilities.Parsers.Fluent;
 using NUnit.Framework;
+using Sprache;
 
 namespace MauMauSharp.Tests.Boards
 {
@@ -13,15 +16,12 @@ namespace MauMauSharp.Tests.Boards
         public void Cards_Are_Shuffled_And_The_Top_Card_Revealed_On_A_Fresh_Board()
         {
             var board = new Board(
-                // TODO: Parser for easy creation
-                new Card[]
-                {
-                    new(Rank.King, Suit.Clubs),
-                    new(Rank.King, Suit.Diamonds)
-                },
+                Deck.FromStacked(
+                    "Kc",
+                    "Kd"),
                 ShufflerMocks.Reversing().Object);
 
-            Assert.That(board.TopPlayedCard(), Is.EqualTo(new Card(Rank.King, Suit.Clubs)));
+            Assert.That(board.TopPlayedCard(), Is.EqualTo(Grammar.Card().Parse("Kc")));
         }
     }
 }
