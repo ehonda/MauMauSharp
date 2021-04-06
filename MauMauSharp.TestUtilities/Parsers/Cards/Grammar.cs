e686e1ba2e8ff36e2dc7a7d599776f8cad333dd0
Dennis Renz <dennis@r-renz.de>
2021-04-06 16:01:02 +0200

    Add canonical card grammar

---
+++ MauMauSharp.TestUtilities/Parsers/Cards/Grammar.cs
@@ -0,0 +1,30 @@
+﻿using MauMauSharp.Cards;
+using MauMauSharp.Cards.Enums;
+using Sprache;
+using System;
+using System.Linq;
+
+namespace MauMauSharp.TestUtilities.Parsers.Cards
+{
+    public static class Grammar
+    {
+        private static Parser<Rank> Rank()
+            => Enum
+                .GetValues<Rank>()
+                .Zip(Enum.GetValues<Rank>().Select(Encoding.RankEncoding))
+                .Select(rankAndEncoding => Parse.Char(rankAndEncoding.Second).Return(rankAndEncoding.First))
+                .Aggregate(Parse.Or);
+
+        private static Parser<Suit> Suit()
+            => Enum
+                .GetValues<Suit>()
+                .Zip(Enum.GetValues<Suit>().Select(Encoding.SuitEncoding))
+                .Select(suitAndEncoding => Parse.Char(suitAndEncoding.Second).Return(suitAndEncoding.First))
+                .Aggregate(Parse.Or);
+
+        public static Parser<Card> Card() =>
+            from rank in Rank()
+            from suit in Suit()
+            select new Card(rank, suit);
+    }
+}
