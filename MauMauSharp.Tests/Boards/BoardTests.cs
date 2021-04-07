efa55d04f845f8f17e023845b331d6eecf9e5955
Dennis Renz <dennis@r-renz.de>
2021-04-07 14:11:53 +0200

    Add method to draw card from supply

--- MauMauSharp.Tests/Boards/BoardTests.cs
+++ MauMauSharp.Tests/Boards/BoardTests.cs
@@ -33,5 +33,35 @@ namespace MauMauSharp.Tests.Boards
 
             Assert.That(board.TopPlayedCard(), Is.EqualTo(Card.From("Qh")));
         }
+
+        [Test]
+        public void The_Top_Card_Of_The_Supply_Can_Be_Drawn()
+        {
+            var board = new Board(
+                Deck.TopDown(
+                    "Kc",
+                    "Kd",
+                    "Qh"),
+                ShufflerMocks.NonShuffling().Object);
+
+            Assert.That(board.DrawCardFromSupply(), Is.EqualTo(Card.From("Kd")));
+        }
+
+        [Test]
+        public void If_A_Card_Is_Drawn_From_An_Empty_Supply_It_Will_Be_Replenished_From_The_Shuffled_Played_Tail()
+        {
+            var board = new Board(
+                Deck.TopDown(
+                    "Kc"),
+                ShufflerMocks.Reversing().Object);
+
+            board.PlayCard(Card.From("Qh"));
+            board.PlayCard(Card.From("Ts"));
+
+            var drawnCard = board.DrawCardFromSupply();
+
+            Assert.That(board.TopPlayedCard(), Is.EqualTo(Card.From("Ts")));
+            Assert.That(drawnCard, Is.EqualTo(Card.From("Qh")));
+        }
     }
 }
--- MauMauSharp/Boards/Board.cs
+++ MauMauSharp/Boards/Board.cs
@@ -8,8 +8,8 @@ namespace MauMauSharp.Boards
     {
         private readonly IShuffler _shuffler;
 
-        private readonly Stack<Card> _played;
-        private readonly Stack<Card> _supply;
+        private Stack<Card> _played;
+        private Stack<Card> _supply;
 
         public Board(IEnumerable<Card> cards, IShuffler shuffler)
         {
@@ -23,5 +23,21 @@ namespace MauMauSharp.Boards
         public Card TopPlayedCard() => _played.Peek();
 
         public void PlayCard(Card card) => _played.Push(card);
+
+        public Card DrawCardFromSupply()
+        {
+            if (_supply.Count == 0)
+                ReplenishSupply();
+
+            return _supply.Pop();
+        }
+
+        private void ReplenishSupply()
+        {
+            var topPlayed = _played.Pop();
+            _supply = new(_shuffler.Shuffle(_played));
+            _played = new();
+            _played.Push(topPlayed);
+        }
     }
 }
