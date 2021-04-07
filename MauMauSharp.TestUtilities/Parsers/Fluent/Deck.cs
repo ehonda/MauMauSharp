52596575597096c23850a082910cb3a198f40748
Dennis Renz <dennis@r-renz.de>
2021-04-07 13:46:01 +0200

    Refactor fluent Deck creation

--- MauMauSharp.TestUtilities/Parsers/Fluent/Deck.cs
+++ MauMauSharp.TestUtilities/Parsers/Fluent/Deck.cs
@@ -9,7 +9,10 @@ namespace MauMauSharp.TestUtilities.Parsers.Fluent
     [PublicAPI]
     public static class Deck
     {
-        public static IEnumerable<MauMauSharp.Cards.Card> FromStacked(params string[] cards)
+        public static IEnumerable<MauMauSharp.Cards.Card> BottomUp(params string[] cards)
             => cards.Select(card => Grammar.Card().Parse(card));
+
+        public static IEnumerable<MauMauSharp.Cards.Card> TopDown(params string[] cards)
+            => BottomUp(cards).Reverse();
     }
 }
--- MauMauSharp.Tests/Boards/BoardTests.cs
+++ MauMauSharp.Tests/Boards/BoardTests.cs
@@ -12,12 +12,12 @@ namespace MauMauSharp.Tests.Boards
         public void Cards_Are_Shuffled_And_The_Top_Card_Revealed_On_A_Fresh_Board()
         {
             var board = new Board(
-                Deck.FromStacked(
+                Deck.TopDown(
                     "Kc",
                     "Kd"),
                 ShufflerMocks.Reversing().Object);
 
-            Assert.That(board.TopPlayedCard(), Is.EqualTo(Card.From("Kc")));
+            Assert.That(board.TopPlayedCard(), Is.EqualTo(Card.From("Kd")));
         }
     }
 }
