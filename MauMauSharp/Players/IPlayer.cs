bee644200af010dea12d929d65510d2c58301ae9
Dennis Renz <dennis@r-renz.de>
2021-04-08 18:44:50 +0200

    Add immutable Hand property

--- MauMauSharp/Players/IPlayer.cs
+++ MauMauSharp/Players/IPlayer.cs
@@ -1,10 +1,13 @@
 ﻿using MauMauSharp.Cards;
 using MauMauSharp.Games;
+using System.Collections.Immutable;
 
 namespace MauMauSharp.Players
 {
     public interface IPlayer
     {
+        public ImmutableArray<Card> Hand { get; }
+
         // TODO: Use Maybe<Card> here
         public Card? PassOrPlayCard(GameState gameState);
     }
--- MauMauSharp/Players/Player.cs
+++ MauMauSharp/Players/Player.cs
@@ -1,11 +1,27 @@
 ﻿using MauMauSharp.Cards;
 using MauMauSharp.Games;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace MauMauSharp.Players
 {
     public abstract class Player : IPlayer
     {
+        private readonly List<Card> _hand;
+
+        protected Player(IEnumerable<Card> hand)
+        {
+            _hand = hand.ToList();
+        }
+
         /// <inheritdoc />
-        public Card? PassOrPlayCard(GameState gameState) => null;
+        public ImmutableArray<Card> Hand => _hand.ToImmutableArray();
+
+        /// <inheritdoc />
+        public Card? PassOrPlayCard(GameState gameState)
+            => ChooseCardToPlay(gameState);
+
+        protected abstract Card ChooseCardToPlay(GameState gameState);
     }
 }
