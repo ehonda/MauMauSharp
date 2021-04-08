56bff2c45d5966d1a4f597d2d374c4bd54895122
Dennis Renz <dennis@r-renz.de>
2021-04-08 18:16:42 +0200

    Add abstract player base class

---
+++ MauMauSharp/Players/Player.cs
@@ -0,0 +1,11 @@
+﻿using MauMauSharp.Cards;
+using MauMauSharp.Games;
+
+namespace MauMauSharp.Players
+{
+    public abstract class Player : IPlayer
+    {
+        /// <inheritdoc />
+        public Card? PassOrPlayCard(GameState gameState) => null;
+    }
+}
