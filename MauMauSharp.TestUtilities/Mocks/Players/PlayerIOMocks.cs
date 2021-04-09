e036c3aabe7ae6d6abc1c7acc3e79692de967c64
Dennis Renz <dennis@r-renz.de>
2021-04-09 19:12:17 +0200

    Add PlayerIO Mocks

---
+++ MauMauSharp.TestUtilities/Mocks/Players/PlayerIOMocks.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using MauMauSharp.Cards;
+using MauMauSharp.Games;
+using MauMauSharp.Players;
+using Moq;
+using System.Collections.Immutable;
+
+namespace MauMauSharp.TestUtilities.Mocks.Players
+{
+    [PublicAPI]
+    public static class PlayerIOMocks
+    {
+        public static Mock<IPlayerIO> ChoosingCardToPlay(Card card)
+        {
+            var playerIO = new Mock<IPlayerIO>();
+            playerIO
+                .Setup(p => p.ChooseCardToPlayOrPass(
+                    It.IsAny<GameState>(),
+                    It.IsAny<ImmutableArray<Card>>()))
+                .Returns(card);
+
+            return playerIO;
+        }
+
+        public static Mock<IPlayerIO> Passing()
+            => ChoosingCardToPlay(null!);
+    }
+}
--- MauMauSharp.Tests/Players/PlayerTests.cs
+++ MauMauSharp.Tests/Players/PlayerTests.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using MauMauSharp.Cards;
+﻿using MauMauSharp.Cards;
 using MauMauSharp.Cards.Enums;
-using MauMauSharp.Games;
 using MauMauSharp.Players;
-using Moq;
+using MauMauSharp.TestUtilities.Mocks.Players;
 using NUnit.Framework;
-using System.Collections.Immutable;
+using System;
 using System.Linq;
-
 using FCard = MauMauSharp.TestUtilities.Parsers.Fluent.Card;
 
 namespace MauMauSharp.Tests.Players
@@ -18,15 +15,9 @@ namespace MauMauSharp.Tests.Players
         [Test]
         public void If_A_Player_Passes_Null_Is_Returned()
         {
-            // TODO: PlayerIO Mocks
-            var playerIo = new Mock<IPlayerIO>();
-            playerIo
-                .Setup(p => p.ChooseCardToPlayOrPass(
-                    It.IsAny<GameState>(),
-                    It.IsAny<ImmutableArray<Card>>()))
-                .Returns<Card?>(null);
-
-            var player = new Player(playerIo.Object, Enumerable.Empty<Card>());
+            var player = new Player(
+                PlayerIOMocks.Passing().Object,
+                Enumerable.Empty<Card>());
 
             // TODO: Arbitrary GameState
             Assert.That(player.PassOrPlayCard(new(new(new(Rank.King, Suit.Clubs), 0))), Is.Null);
@@ -35,15 +26,9 @@ namespace MauMauSharp.Tests.Players
         [Test]
         public void If_A_Player_Chooses_To_Play_A_Card_That_Is_Not_In_Hand_An_Exception_Is_Thrown()
         {
-            // TODO: PlayerIO Mocks
-            var playerIo = new Mock<IPlayerIO>();
-            playerIo
-                .Setup(p => p.ChooseCardToPlayOrPass(
-                    It.IsAny<GameState>(),
-                    It.IsAny<ImmutableArray<Card>>()))
-                .Returns(FCard.From("Qc"));
-
-            var player = new Player(playerIo.Object, Enumerable.Empty<Card>());
+            var player = new Player(
+                PlayerIOMocks.ChoosingCardToPlay(FCard.From("Qc")).Object,
+                Enumerable.Empty<Card>());
 
             Assert.That(() => player.PassOrPlayCard(new(new(new(Rank.King, Suit.Clubs), 0))),
                 Throws.TypeOf<InvalidOperationException>()
