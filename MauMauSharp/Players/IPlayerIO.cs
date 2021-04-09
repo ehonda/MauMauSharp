5977136770df591edd3f65cce50f608cb0f47227
Dennis Renz <dennis@r-renz.de>
2021-04-09 19:26:18 +0200

    Add Note to IPlayerIO about post conditions

--- MauMauSharp/Players/IPlayerIO.cs
+++ MauMauSharp/Players/IPlayerIO.cs
@@ -5,10 +5,11 @@ using System.Collections.Immutable;
 namespace MauMauSharp.Players
 {
     // TODO: Better name!
-    // ReSharper disable once InconsistentNaming
     public interface IPlayerIO
     {
         // TODO: Can probably pass hand as part of GameState?
+        // NOTE: This must return a card that is a) in hand b) valid to play,
+        //       otherwise upstream (Player / Board) will throw
         public Card? ChooseCardToPlayOrPass(GameState gameState, ImmutableArray<Card> hand);
     }
 }
