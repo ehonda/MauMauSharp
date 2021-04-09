5475cb7ad295a1fe5900b787f5b022a770927dd4
Dennis Renz <dennis@r-renz.de>
2021-04-10 01:14:33 +0200

    Add fluent Hand parsing syntax

---
+++ MauMauSharp.TestUtilities/Parsers/Fluent/Hand.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using MauMauSharp.TestUtilities.Parsers.Cards;
+using Sprache;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MauMauSharp.TestUtilities.Parsers.Fluent
+{
+    [PublicAPI]
+    public static class Hand
+    {
+        public static IEnumerable<MauMauSharp.Cards.Card> From(string hand)
+            => Grammar.Hand().Parse(hand);
+
+        public static IEnumerable<MauMauSharp.Cards.Card> Empty()
+            => Enumerable.Empty<MauMauSharp.Cards.Card>();
+    }
+}
