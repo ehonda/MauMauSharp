98f9fb993c95cc6dda0de41b131c38f580d2855c
Dennis Renz <dennis@r-renz.de>
2021-04-09 19:17:25 +0200

    Add BoardState data with arbitrary values

---
+++ MauMauSharp.TestUtilities/Data/Boards/BoardStateData.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using MauMauSharp.Boards;
+using MauMauSharp.TestUtilities.Parsers.Fluent;
+
+namespace MauMauSharp.TestUtilities.Data.Boards
+{
+    [PublicAPI]
+    public static class BoardStateData
+    {
+        public static BoardState WithArbitraryValues()
+            => new(Card.From("As"), 0);
+    }
+}
