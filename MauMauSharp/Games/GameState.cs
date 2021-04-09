9a60f4cf6ff036b72aa3c2a1eb7acf814fbad766
Dennis Renz <dennis@r-renz.de>
2021-04-09 19:24:20 +0200

    Add TODO about missing values to add to GameState

--- MauMauSharp/Games/GameState.cs
+++ MauMauSharp/Games/GameState.cs
@@ -5,5 +5,6 @@ namespace MauMauSharp.Games
     // TODO: Other values that should be included in GameState:
     //          - The player's hand (As Value object? / ValueSequence?)
     //          - The number of cards in the other player's hands
+    //          - A list of all playable cards / current valid move validator
     public record GameState(BoardState BoardState);
 }
