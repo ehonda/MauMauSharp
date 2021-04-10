55ceb8b17c1127c528ecefab880a70d1bfb31360
Dennis Renz <dennis@r-renz.de>
2021-04-10 12:59:07 +0200

    Add fluent Game from Mocks creation

---
+++ MauMauSharp.TestUtilities/Mocks/Fluent/Game.cs
@@ -0,0 +1,14 @@
+﻿using MauMauSharp.Boards;
+using MauMauSharp.Players;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MauMauSharp.TestUtilities.Mocks.Fluent
+{
+    public static class Game
+    {
+        public static Games.Game FromMocks(Mock<IBoard> board, IEnumerable<Mock<IPlayer>> players)
+            => new(board.Object, players.Select(player => player.Object));
+    }
+}
