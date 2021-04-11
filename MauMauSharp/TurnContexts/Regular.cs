60bba09e8872946ee2760c0156efbbb2b1b3c34f
Dennis Renz <dennis@r-renz.de>
2021-04-11 16:16:03 +0200

    Add context for regular turns

---
+++ MauMauSharp/TurnContexts/Regular.cs
@@ -0,0 +1,29 @@
+﻿using MauMauSharp.Cards;
+using MauMauSharp.Players;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace MauMauSharp.TurnContexts
+{
+    public class Regular : ITurnContext
+    {
+        /// <inheritdoc />
+        public ImmutableArray<Card> PlayableCards { get; }
+
+        /// <inheritdoc />
+        public int CardsToDrawOnPass { get; }
+
+        public Regular(IEnumerable<Card> playableCards, int cardsToDrawOnPass)
+        {
+            PlayableCards = playableCards.ToImmutableArray();
+            CardsToDrawOnPass = cardsToDrawOnPass;
+        }
+
+        /// <inheritdoc />
+        public ITurnContext NextTurnContext(Card? playedCard, IPlayer activePlayer)
+            => playedCard switch
+            {
+                null => new Regular(PlayableCards, 1)
+            };
+    }
+}
