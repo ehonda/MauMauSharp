e5ba5ea8eca85987c9d1ddb8272029fcccd7a75d
Dennis Renz <dennis@r-renz.de>
2021-04-11 20:05:29 +0200

    Add cards to draw and playable cards tests for Ace

---
+++ MauMauSharp.Tests/TurnContexts/AceTests.cs
@@ -0,0 +1,23 @@
+﻿using MauMauSharp.TurnContexts;
+using NUnit.Framework;
+
+namespace MauMauSharp.Tests.TurnContexts
+{
+    [TestFixture]
+    public class AceTests
+    {
+        [Test]
+        public void Zero_Cards_Are_Drawn_On_An_Ace_Turn_Pass()
+        {
+            var ace = new Ace();
+            Assert.That(ace.CardsToDrawOnPass, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void No_Cards_Are_Playable_On_An_Ace_Turn()
+        {
+            var ace = new Ace();
+            Assert.That(ace.PlayableCards, Is.Empty);
+        }
+    }
+}
--- MauMauSharp/TurnContexts/Ace.cs
+++ MauMauSharp/TurnContexts/Ace.cs
@@ -7,10 +7,10 @@ namespace MauMauSharp.TurnContexts
     public class Ace : ITurnContext
     {
         /// <inheritdoc />
-        public ImmutableArray<Card> PlayableCards { get; }
+        public ImmutableArray<Card> PlayableCards { get; } = ImmutableArray<Card>.Empty;
 
         /// <inheritdoc />
-        public int CardsToDrawOnPass { get; }
+        public int CardsToDrawOnPass { get; } = 0;
 
         /// <inheritdoc />
         public ITurnContext NextTurnContext(Card? playedCard, IPlayer activePlayer) => null;
