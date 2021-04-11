f6ec8e91c5bfb1c428cc2c8531470cfad921b2c1
Dennis Renz <dennis@r-renz.de>
2021-04-11 15:53:32 +0200

    Add Turn Context interface

---
+++ MauMauSharp/Turns/ITurnContext.cs
@@ -0,0 +1,15 @@
+﻿using MauMauSharp.Cards;
+using MauMauSharp.Players;
+using System.Collections.Immutable;
+
+namespace MauMauSharp.Turns
+{
+    public interface ITurnContext
+    {
+        public ImmutableArray<Card> PlayableCards { get; }
+
+        public int CardsToDrawOnPass { get; }
+
+        public ITurnContext NexTurnContext(Card? playedCard, IPlayer activePlayer);
+    }
+}
