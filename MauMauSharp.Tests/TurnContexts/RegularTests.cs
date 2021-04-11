fdc3a29f45f0fad552f596050a974d358d7f65f1
Dennis Renz <dennis@r-renz.de>
2021-04-11 18:20:35 +0200

    Add test for passed regular turn next turn

--- MauMauSharp.Tests/TurnContexts/RegularTests.cs
+++ MauMauSharp.Tests/TurnContexts/RegularTests.cs
@@ -1,7 +1,9 @@
-﻿using MauMauSharp.Cards;
-using MauMauSharp.TurnContexts;
+﻿using MauMauSharp.TurnContexts;
 using NUnit.Framework;
 using System.Linq;
+using MauMauSharp.TestUtilities.Mocks.Players;
+using MauMauSharp.TestUtilities.Parsers.Fluent;
+using Card = MauMauSharp.Cards.Card;
 
 namespace MauMauSharp.Tests.TurnContexts
 {
@@ -13,6 +15,15 @@ namespace MauMauSharp.Tests.TurnContexts
         {
             var regular = new Regular(Enumerable.Empty<Card>());
             Assert.That(regular.CardsToDrawOnPass, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void If_A_Regular_Turn_Is_Passed_The_Next_Turn_Is_Regular_With_The_Same_Playable_Cards()
+        {
+            var first = new Regular(Hand.From("As Ks Qs Js Ts 9s 8s 7s 8d 8c 8h Jd Jc Jh"));
+            var second = first.NextTurnContext(null, PlayerMocks.Arbitrary().Object);
+
+            Assert.That(first.PlayableCards, Is.EqualTo(second.PlayableCards));
         }
     }
 }
