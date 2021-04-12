b4153b37d02935671fe8d2c6245e978e1d7013f3
Dennis Renz <dennis@r-renz.de>
2021-04-12 22:19:21 +0200

    Add Seven turn context

---
+++ MauMauSharp/TurnContexts/Seven.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Immutable;
+using MauMauSharp.Cards;
+using MauMauSharp.Players;
+
+namespace MauMauSharp.TurnContexts
+{
+    public class Seven : ITurnContext
+    {
+        /// <inheritdoc />
+        public ImmutableArray<Card> PlayableCards { get; }
+
+        /// <inheritdoc />
+        public int CardsToDrawOnPass { get; }
+
+        /// <inheritdoc />
+        public ITurnContext NextTurnContext(Card? playedCard, IPlayer activePlayer) => null;
+    }
+}
