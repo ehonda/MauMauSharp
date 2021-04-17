d18edf67b9e68b9dbf96b511bf548fef2bbf30f0
Dennis Renz <dennis@r-renz.de>
2021-04-17 11:41:44 +0200

    Add CardData

---
+++ MauMauSharp.TestUtilities/Data/Cards/CardData.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using MauMauSharp.Cards;
+using MauMauSharp.Cards.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MauMauSharp.TestUtilities.Data.Cards
+{
+    // TODO: There's some duplication between this and main projects' Deck class - Unify!
+    [PublicAPI]
+    public static class CardData
+    {
+        public static IEnumerable<Card> AllCards()
+            => Enum
+                .GetValues<Suit>()
+                .SelectMany(suit => Enum
+                    .GetValues<Rank>()
+                    .Select(rank => new Card(rank, suit)));
+
+        public static IEnumerable<Card> AllCardsOfRank(Rank rank)
+            => Enum
+                .GetValues<Suit>()
+                .Select(suit => new Card(rank, suit));
+
+        public static IEnumerable<Card> AllButRank(Rank rank)
+            => AllCards().Except(AllCardsOfRank(rank));
+    }
+}
