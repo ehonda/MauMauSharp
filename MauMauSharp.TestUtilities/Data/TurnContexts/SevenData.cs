4f06b6b7222073121e6e87b8aad546c61e3fc711
Dennis Renz <dennis@r-renz.de>
2021-04-17 11:28:04 +0200

    Comment SevenData functions for clarity

--- MauMauSharp.TestUtilities/Data/TurnContexts/SevenData.cs
+++ MauMauSharp.TestUtilities/Data/TurnContexts/SevenData.cs
@@ -21,27 +21,51 @@ namespace MauMauSharp.TestUtilities.Data.TurnContexts
                 .Select(suit => new Card(Rank.Seven, suit));
 
         // TODO: Better name!
+        // Example return value (S is one consecutive 7-turn, S S is 2 etc):
+        // [ S, S S, S S S ]
         public static IEnumerable<ITurnContext> NthConsecutiveSevenTurnOneToCount(int count)
-            => Enumerable
-                .Range(1, count)
-                .Select(NthConsecutiveSevenTurn);
+            => ConsecutiveSevensOneToCount(count)
+                .Select(ToNthConsecutiveSevenTurn);
 
+        // Example return value:
+        // S S S
         public static ITurnContext NthConsecutiveSevenTurn(int count)
+            => ToNthConsecutiveSevenTurn(ConsecutiveSevens(count));
+
+        // TODO: Needed? If not, remove
+        // Example conversion:
+        // [ 7, 7, 7 ] -> [ S, S S, S S S ]
+        public static IEnumerable<ITurnContext> ToSevenTurnSequence(IEnumerable<Card> sevens)
         {
-            var sevens = ConsecutiveSevens(count).ToImmutableArray();
-            return sevens
+            var sevensArray = sevens.ToImmutableArray();
+            return Enumerable
+                .Range(1, sevensArray.Length)
+                .Select(count => sevensArray.Take(count))
+                .Select(ToNthConsecutiveSevenTurn);
+        }
+
+        // Example conversion:
+        // [ 7, 7, 7 ] -> S S S
+        public static ITurnContext ToNthConsecutiveSevenTurn(IEnumerable<Card> sevens)
+        {
+            var sevensArray = sevens.ToImmutableArray();
+            return sevensArray
                 .Skip(1)
                 .Aggregate(
-                    new Seven(sevens.First()) as ITurnContext,
+                    new Seven(sevensArray.First()) as ITurnContext,
                     (turn, card) => turn.NextTurnContext(card, PlayerMocks.Arbitrary().Object));
         }
 
         // TODO: Better name!
+        // Example return value:
+        // [ [ 7 ], [ 7, 7 ], [ 7, 7, 7 ] ]
         public static IEnumerable<IEnumerable<Card>> ConsecutiveSevensOneToCount(int count)
             => Enumerable
                 .Range(1, count)
                 .Select(ConsecutiveSevens);
 
+        // Example return value:
+        // [ 7, 7, 7, 7 ]
         private static IEnumerable<Card> ConsecutiveSevens(int count)
             => AllSevens()
                 .Cycle()
