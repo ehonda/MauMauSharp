5295e5e914fb468ebe46c420b2ace53957f20df3
Dennis Renz <dennis@r-renz.de>
2021-04-17 13:44:11 +0200

    Add Jack turn context

---
+++ MauMauSharp/TurnContexts/Jack.cs
@@ -0,0 +1,18 @@
+﻿using MauMauSharp.Cards;
+using MauMauSharp.Players;
+using System.Collections.Immutable;
+
+namespace MauMauSharp.TurnContexts
+{
+    public class Jack : ITurnContext
+    {
+        /// <inheritdoc />
+        public ImmutableArray<Card> PlayableCards { get; }
+
+        /// <inheritdoc />
+        public int CardsToDrawOnPass { get; }
+
+        /// <inheritdoc />
+        public ITurnContext NextTurnContext(Card? playedCard, IPlayer activePlayer) => null;
+    }
+}
