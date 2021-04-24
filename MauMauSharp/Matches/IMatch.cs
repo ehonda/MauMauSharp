0b4c138e26265dcf4abd511d7565ec1f578fdc28
Dennis Renz <dennis@r-renz.de>
2021-04-24 12:58:55 +0200

    Add IMatch (prototype)

---
+++ MauMauSharp/Matches/IMatch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MauMauSharp.Players;
+
+namespace MauMauSharp.Matches
+{
+    public interface IMatch
+    {
+        // TODO: Unsure if this is the right signature:
+        //          - We might want to pass a ranking of players to get 3rd, 2nd, 1st finisher
+        //          - We might want to pass players on construction
+        //          - ...
+        public IPlayer Play(IEnumerable<IPlayer> players);
+    }
+}
