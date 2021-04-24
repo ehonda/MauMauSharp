c68e525ea36f005292d4fdc5738c5190c48604b1
Dennis Renz <dennis@r-renz.de>
2021-04-24 11:28:53 +0200

    Add player mock tracking hand, with play sequence

--- MauMauSharp.TestUtilities/Mocks/Players/PlayerMocks.cs
+++ MauMauSharp.TestUtilities/Mocks/Players/PlayerMocks.cs
@@ -5,12 +5,49 @@ using MauMauSharp.Games;
 using MauMauSharp.Players;
 using MauMauSharp.TestUtilities.Extensions;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace MauMauSharp.TestUtilities.Mocks.Players
 {
     [PublicAPI]
     public static class PlayerMocks
     {
+        public static Mock<IPlayer> WithInitialHandAndPlaySequence(
+            IEnumerable<Card> hand,
+            IEnumerable<Card?> playSequence)
+        {
+            var mock = new Mock<IPlayer>();
+            var handList = hand.ToList();
+
+            mock
+                .Setup(player => player.Hand)
+                .Returns(() => handList.ToImmutableArray());
+
+            mock
+                .Setup(player => player.TakeCard(It.IsAny<Card>()))
+                .Callback<Card>(card => handList.Add(card));
+            
+            var playSequenceEnumerator = playSequence.GetEnumerator();
+            mock
+                .Setup(player => player.PassOrPlayCard(It.IsAny<GameState>()))
+                .Returns(() =>
+                {
+                    playSequenceEnumerator.MoveNext();
+                    var cardToPlay = playSequenceEnumerator.Current;
+
+                    if (cardToPlay is not null && handList.Contains(cardToPlay) is false)
+                        throw new InvalidOperationException(
+                            $"Card in play sequence is not in hand: {cardToPlay}");
+
+                    return cardToPlay;
+                });
+
+            return mock;
+        }
+
         public static Mock<IPlayer> PlayingCard(Card card)
         {
             var mock = new Mock<IPlayer>();
